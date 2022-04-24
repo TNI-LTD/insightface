ae83a725f31c7d168cc10d593b7d699865e04b73
AntiAegis <thuypn9a4@gmail.com>
2022-04-24 09:26:22 +0700

    initial upload

---
+++ insightface/__init__.py
@@ -0,0 +1,9 @@
+import onnxruntime
+
+from . import model_zoo
+from . import utils
+from . import app
+from . import data
+from . import thirdparty
+
+__version__ = '0.6.2'
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='insightface',
+      packages=['insightface'],
+      version='0.6.2',
+      zip_safe=False)
